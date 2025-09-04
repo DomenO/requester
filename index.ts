165d3815356907575667bf2a7311c95e6675117e
Alexey Formin <domen-oo@yandex.ru>
2025-09-04 21:09:34 +0300

    add landing

--- index.ts
+++ index.ts
@@ -1,17 +1,25 @@
 import { serve } from "bun";
 
+const landingHTML = await Bun.file("landing.html").text();
+
 serve({
   idleTimeout: 0,
   fetch(req) {
     const url = new URL(req.url);
     const params = url.searchParams;
 
-    // Получаем параметры
+    if (url.pathname === "/" && params.toString() === "") {
+      return new Response(landingHTML, {
+        headers: {
+          "Content-Type": "text/html; charset=utf-8"
+        },
+      });
+    }
+
     const pending = parseInt(params.get("pending") || "0", 10);
     const status = parseInt(params.get("status") || "200", 10);
     const body = params.get("body") || "";
-    
-    // Заголовки
+
     const headers = new Headers();
 
     headers.set("Access-Control-Allow-Origin", "*");
@@ -24,7 +32,6 @@ serve({
       }
     });
 
-    // Ответ с задержкой
     return new Promise<Response>((resolve) => {
       setTimeout(() => {
         resolve(new Response(body, { status, headers }));
